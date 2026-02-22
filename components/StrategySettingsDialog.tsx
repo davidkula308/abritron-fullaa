1b34cd6cbf3e698713417fa0690c00a9ed7fc3fb
davidkula308 <120595291+davidkula308@users.noreply.github.com>
2026-02-22 15:51:58 +0300

    Delete components directory

--- components/StrategySettingsDialog.tsx
+++
@@ -1,6 +0,0 @@
-// This component is a placeholder for future strategy settings editing
-// Currently, editing is handled by re-opening the builder dialogs
-
-export default function StrategySettingsDialog() {
-  return null;
-}
